a3363caca63100a2fe24ec1bda8cbbaa4434c017
James Salter <iteration@gmail.com>
2017-03-16 23:09:58 +0000

    support develop and inplace

--- rust_ext/__init__.py
+++ rust_ext/__init__.py
@@ -5,7 +5,8 @@ import os.path
 import glob
 from distutils.cmd import Command
 from distutils.command.install_lib import install_lib
-import shutil
+from setuptools.command.develop import develop
+from distutils.file_util import copy_file
 
 
 class RustBuildCommand(Command):
@@ -18,7 +19,13 @@ class RustBuildCommand(Command):
 
     description = "build rust crates into Python extensions"
 
-    user_options = []
+    user_options = [
+        ('inplace', 'i',
+         "ignore build-lib and put compiled extensions into the source "
+             "directory alongside your pure Python modules"),
+    ]
+
+    boolean_options = ['inplace']
 
     def _unpack_classargs(self):
         for k, v in self.__class__.args.items():
@@ -26,6 +33,7 @@ class RustBuildCommand(Command):
 
     def initialize_options(self):
         self._unpack_classargs()
+        self.inplace = 0
 
     def finalize_options(self):
         pass
@@ -92,23 +100,31 @@ class RustBuildCommand(Command):
         try:
             dylib_path = glob.glob(os.path.join(target_dir, wildcard_so))[0]
         except IndexError:
-            raise Exception("rust build failed; unable to find any .dylib in %s" %
-                target_dir)
+            raise Exception("rust build failed; unable to find any shared "
+                            "library in %s" % target_dir)
 
         # Ask build_ext where the shared library would go if it had built it,
         # then copy it there.
         build_ext = self.get_finalized_command('build_ext')
-        target_fname = os.path.splitext(os.path.basename(dylib_path)[3:])[0]
+        build_ext.inplace = self.inplace
+        if self.ext_name:
+            target_fname = self.ext_name
+        else:
+            target_fname = os.path.splitext(os.path.basename(dylib_path)[3:])[0]
         ext_path = build_ext.get_ext_fullpath(os.path.basename(target_fname))
+
         try:
             os.makedirs(os.path.dirname(ext_path))
         except OSError:
             pass
-        shutil.copyfile(dylib_path, ext_path)
+
+        copy_file(dylib_path, ext_path, verbose=self.verbose,
+                dry_run=self.dry_run)
 
 
 def build_rust_cmdclass(cargo_toml_path, debug=False, 
-                        extra_cargo_args=None, quiet=False):
+                        extra_cargo_args=None, quiet=False,
+                        ext_name=None):
     """
     Args:
         cargo_toml_path (str)   The path to the cargo.toml manifest
@@ -118,6 +134,9 @@ def build_rust_cmdclass(cargo_toml_path, debug=False,
         extra_carg_args (list)  A list of extra argumenents to be passed to 
                                 cargo.
         quiet (boolean)         If True, doesn't echo cargo's output.
+        ext_name (str)          Name of the extension, including package names.
+                                If not provided, will be the name of the
+                                library built by cargo with 'lib' removed
 
     Returns:
         A Command subclass suitable for passing to the cmdclass argument
@@ -151,3 +170,15 @@ class install_lib_including_rust(install_lib):
         install_lib.build(self)
         if not self.skip_build:
             self.run_command('build_rust')
+
+
+class develop_including_rust(develop):
+    """
+    A replacement develop cmdclass that remembers to build_rust
+    inplace=1 (develop already does build_ext inplace=1)
+    """
+
+    def run(self):
+        develop.run(self)
+        self.reinitialize_command('build_rust', inplace=1)
+        self.run_command('build_rust')
