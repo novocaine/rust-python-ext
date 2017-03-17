fe1a53b8fa67a7b792677c53e77e6ab4221e0476
James Salter <james@memrise.com>
2017-03-17 09:35:43 +0000

    Revert "Revert "support develop and inplace""
    This reverts commit 2065a3d33c4bb490fb7ae60826e891dd0da20094.

--- example/setup.py
+++ example/setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
-from rust_ext import build_rust_cmdclass, install_lib_including_rust
+from rust_ext import build_rust_cmdclass, install_lib_including_rust, \
+    develop_including_rust
 
 setup(name='hello-rust',
     version='1.0',
     cmdclass={
         # This enables 'setup.py build_rust', and makes it run
         # 'cargo extensions/cargo.toml' before building your package.
-        'build_rust': build_rust_cmdclass('extensions/Cargo.toml'),
+        'build_rust': build_rust_cmdclass('extensions/Cargo.toml', 
+                                          ext_name="hello_rust.hellorust"),
         # This causes your rust binary to be automatically installed
         # with the package when install_lib runs (including when you 
         # run 'setup.py install'.
-        'install_lib': install_lib_including_rust
+        'install_lib': install_lib_including_rust,
+        # This supports development mode for your rust extension by 
+        # causing the ext to be built in-place, according to its ext_name
+        'develop': develop_including_rust
     },
     packages=['hello_rust'],
     # rust extensions are not zip safe, just like C-extensions.
